6c26d6ec765880d22faf86ada82bf52b2c72d017
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-11-11 18:38:32 +0530

    hero sectoin complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
 import Header from '@/components/Header'
+import HeroSection from '@/components/Home/HeroSection'
 
 export default function Home() {
 
   return (
     <main>
       <Header />
-      <div>
-        <section className='px-16 h-[550px] flex items-center'>
-          <div>
-            <h1 className='font-extrabold text-[70px] w-[1000px] leading-[80px]'>Unlock the Power of <span className='text-light'>Excelens</span>, Anywhere, Anytime!</h1>
-            <div>
-              <button className='hover:bg-light text-lg rounded-xl text-white px-3 py-2 mt-8 bg-primary'>Get Started</button>
-            </div>
-          </div>
-
-          <img src="/excel-icon.jpg" alt="" className='w-[700px]' />
-
-        </section>
-      </div>
+      <HeroSection />
+      
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import Link from 'next/link'
 function Header() {
     return (
-        <header className='h-20 flex py-16 px-10 justify-between items-center'>
+        <header className='h-20 flex pt-20 pb-10 px-16 justify-between items-center'>
             <div className='w-48'>
                 <img className='object-contain' src="/logo/logo.png" />
             </div>
             <div>
-                <ul className='flex gap-3 items-center'>
-
-                    <li className='hover:text-primary'>
+                <ul className='flex gap-1 items-center'>
+                    
+                    <li className='hover:text-primary px-3 py-1 hover:border rounded-lg'>
                         <Link href={"#"}>Register</Link>
                     </li>
-                    <li className='hover:bg-light rounded-xl text-white px-2 py-1 bg-primary'>
+                    <li className='hover:bg-light border-primary border rounded-lg text-white px-3 py-1 bg-primary'>
                         <Link href={"#"}>Login</Link>
                     </li>
                 </ul>
