6a20f32c9e5fa55dd6ec8c95657a907add109b56
MUHAMMED HISHAM AC <122851090+hishamac@users.noreply.github.com>
2023-11-11 08:47:47 +0000

    commit

--- src/app/(user)/signin/page.tsx
+++ src/app/(user)/signin/page.tsx
@@ -1,10 +1,35 @@
-export default function Signin(){
-    const
-    return <div>
-        <form action="">
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <button type="submit">Sign In</button>
-        </form>
+"use client";
+import { useState } from "react";
+
+export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitForm = (e: any) => {
+    e.preventDefault();
+    console.log({
+      email: email,
+      password: password,
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e: any) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e: any) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign In</button>
+      </form>
     </div>
+  );
 }
--- src/app/(user)/signup/page.tsx
+++ src/app/(user)/signup/page.tsx
@@ -1,26 +1,57 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
-export default function  Signup(){
-    const [avatar,setAvatar] = useState(null)
-    const [name,setName]= useState("")
-    const [email,setEmail]= useState("")
-    const [password,setPassword]= useState("")
+export default function Signup() {
+  const [avatar, setAvatar] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const submitForm = (e:any)=>{
-        e.preventDefault()
-        console.log({avatar,name,email,password});
-    }
-    return(
-        <div>
-        <form onSubmit={submitForm}>
-            <input type="file" placeholder="avatar" onChange={(e:any)=>{setAvatar((e.target.files)[0])}} required/>
-            <input type="text" placeholder="name" onChange={(e:any)=>{setName(e.target.value)}} required/>
-            <input type="email" placeholder="email" onChange={(e:any)=> {setEmail(e.target.value)}} required/>
-            <input type="password" placeholder="password" onChange={(e:any)=>{e.target.value}} required/>
-            <button type="submit">Sign Up</button>
-        </form>
-        </div>
-    )
+  const submitForm = (e: any) => {
+    e.preventDefault();
+    console.log({
+      avatar: avatar,
+      name: name,
+      email: email,
+      password: password,
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <input
+          type="file"
+          placeholder="avatar"
+          onChange={(e: any) => {
+            setAvatar(e.target.files[0]);
+          }}
+          required
+        />
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e: any) => {
+            setName(e.target.value);
+          }}
+          required
+        />
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e: any) => {
+            setEmail(e.target.value);
+          }}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e: any) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
