1df61e85153a406ad25df5f070b26994db861419
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-11-11 16:35:26 +0530

    public created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Header from '@/components/Header'
+
 export default function Home() {
+
   return (
-  <main className="bg-primary">
-    Home
-  </main>
+    <main>
+      <Header />
+      <div> body</div>
+    </main>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+function Header() {
+  return (
+    <header className='h-20 flex'>
+        <img src={"@@/logo/logo.png"}/>
+    </header>
+  )
+}
+
+export default Header
