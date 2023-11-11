af05a00dc2087ecfcda2848ffc1f3b69e71cfbbc
MUHAMMED HISHAM AC <122851090+hishamac@users.noreply.github.com>
2023-11-11 08:05:56 +0000

    First Commit

---
+++ src/app/(user)/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function Signin(){
+    const
+    return <div>
+        <form action="">
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button type="submit">Sign In</button>
+        </form>
+    </div>
+}
