354f44bf16d7b3e8f9bb91c76628ac47d8647e58
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-11-11 17:17:29 +0530

    home started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,19 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <div> body</div>
+      <div>
+        <section className='px-16 h-[550px] flex items-center'>
+          <div>
+            <h1 className='font-extrabold text-[70px] w-[1000px] leading-[80px]'>Unlock the Power of <span className='text-light'>Excelens</span>, Anywhere, Anytime!</h1>
+            <div>
+              <button className='hover:bg-light text-lg rounded-xl text-white px-3 py-2 mt-8 bg-primary'>Get Started</button>
+            </div>
+          </div>
+
+          <img src="/excel-icon.jpg" alt="" className='w-[700px]' />
+
+        </section>
+      </div>
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import Image from 'next/image'
+import Link from 'next/link'
 function Header() {
-  return (
-    <header className='h-20 flex'>
-        <img src={"@@/logo/logo.png"}/>
-    </header>
-  )
+    return (
+        <header className='h-20 flex py-16 px-10 justify-between items-center'>
+            <div className='w-48'>
+                <img className='object-contain' src="/logo/logo.png" />
+            </div>
+            <div>
+                <ul className='flex gap-3 items-center'>
+
+                    <li className='hover:text-primary'>
+                        <Link href={"#"}>Register</Link>
+                    </li>
+                    <li className='hover:bg-light rounded-xl text-white px-2 py-1 bg-primary'>
+                        <Link href={"#"}>Login</Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    )
 }
 
 export default Header
