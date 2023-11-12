7aba4a697e8211cac4b81f387e26504fde93d2dd
Muhammed Zulfikar <99268671+zzulfi@users.noreply.github.com>
2023-11-12 04:09:57 +0000

    chagn

---
+++ src/pages/api/excel/download.ts
@@ -0,0 +1,13 @@
+// var value:String = value.format?value.result :value
+const aff = [
+  {
+    value: "",
+    cellAddress:"",
+    format: {
+      color: "#0000",
+      bold: true,
+      italic: true,
+    },
+    formula:""
+  },
+];
