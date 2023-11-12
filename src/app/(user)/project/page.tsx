f2010fae2de7e71cee64501aba0c1acfc3552a08
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-11-12 12:39:57 +0530

    sample data

--- src/app/(user)/project/page.tsx
+++ src/app/(user)/project/page.tsx
@@ -25,27 +25,31 @@ export default function Page() {
     }
 
     return <div className="mt-5">
-        <form onSubmit={submitForm} className="w-full justify-center flex">
-            <input type="file" className="file:border-primary file:border file:rounded-lg file:text-primary file:px-3 file:py-1 file:hover:bg-smoke file:font-semibold file:bg-white" onChange={(e: any) => { setFile(e.target.files[0]) }} required />
-            <input type="submit" className="border-primary border rounded-lg text-white px-3 py-1 hover:bg-light bg-primary" value={`Upload`} />
-    <a href="https://fastupload.io/lXEVV4BKDOLN/NWX1U53KCz8N7SC/rk9zKnMVQ30lY/test.xlsx" className='border-primary border rounded-lg text-white px-3 py-1 hover:bg-light bg-primary ml-2' >Download Demo File</a>
+        <form onSubmit={submitForm} className="w-full justify-center flex flex-col items-center">
+        <div>
+        <input type="file" className="file:border-primary file:border file:rounded-lg file:text-primary file:px-3 file:py-1 file:hover:bg-smoke file:font-semibold file:bg-white" onChange={(e: any) => { setFile(e.target.files[0]) }} required />
+            <input type="submit" className="border-primary border rounded-lg text-white px-3 py-1 hover:bg-light bg-primary" value={`Submit`} />
+            
+        </div>
+            <a href="https://fastupload.io/lXEVV4BKDOLN/NWX1U53KCz8N7SC/rk9zKnMVQ30lY/test.xlsx" className='mt-3 text-primary px-3 border border-white rounded-xl py-1 hover:border-primary ml-2' >Download Demo File</a>
         </form>
         {excelData.map((sheet: any, sheetIndex: any) => (
             <div className=" flex flex-col items-center">
-                <h2>{sheet.sheetName}</h2>
-                <table className="table overflow-auto min-w-[80%] max-w-[90%] max-h-[70vh]">
+                <hr className="border-2 border-dashed w-full border-primary mt-20 -mb-7"/>
+                <h2 className="p-3 bg-primary  rounded-xl w-48 text-white text-lg font-semibold text-center">{sheet.sheetName}</h2>
+                <table className="table mt-10 rounded-xl overflow-hidden min-w-[80%] max-w-[90%] max-h-[70vh]">
                     <thead>
-                        <tr className="border-2 border-dark">
+                        <tr className="border-2 border-smoke">
                             {sheet.headers.map((header: any, rowIndex: any) => (
-                                <td className="border-2 border-dark">{header}</td>
+                                <td className="border-2 p-2 bg-primary text-white capitalize border-primary">{header}</td>
                             ))}
                         </tr>
                     </thead>
                     <tbody>
                         {sheet.rows.map((row: any, rowIndex: any) => (
-                        <tr className="border border-dark">
-                        {row.cells.map((cell: any, cellIndex: any) => (
-                                    <td key={cellIndex} scope="col" className="border border-dark" >{cell.formula ? cell.value.result : cell.value}</td>
+                            <tr className="border border-smoke">
+                                {row.cells.map((cell: any, cellIndex: any) => (
+                                    <td key={cellIndex} scope="col" className="border p-2 border-smoke" >{cell.formula ? cell.value.result : cell.value}</td>
                                 ))}
                             </tr>
                         ))}
--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 function HeroSection() {
     return (
         <div>
@@ -7,7 +7,7 @@ function HeroSection() {
                 <div>
                     <h1 className='font-extrabold text-[39px] leading-[50px] lg:text-[70px] lg:w-[1000px] lg:leading-[80px]'>Unlock the Power of <span className='text-light'>Excelens</span>, Anywhere, Anytime!</h1>
                     <div className='flex gap-2 font-semibold'>
-                        <button className='hover:bg-light text-lg rounded-lg text-white px-3 py-2 mt-8 bg-primary'>Get Started</button>
+                    <Link href={"/project"}><button className='hover:bg-light text-lg rounded-lg text-white px-3 py-2 mt-8 bg-primary'>Try Demo</button></Link>
                         <button className='hover:text-light border border-primary hover:border-light text-lg rounded-lg text-primary px-3 py-2 mt-8'>Read Docs</button>
                     </div>
                 </div>
