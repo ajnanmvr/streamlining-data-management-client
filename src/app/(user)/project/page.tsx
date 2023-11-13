40e9110c27c05834d2503bd0e3ef8d8e9fddf23b
Muhammed Zulfikar <99268671+zzulfi@users.noreply.github.com>
2023-11-13 17:36:32 +0000

    added download

--- src/app/(user)/project/page.tsx
+++ src/app/(user)/project/page.tsx
@@ -1,104 +1,151 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { usePathname, useRouter } from "next/navigation";
+
+const downloadExcelFile = async (excelData: String) => {
+  try {
+    const postData = {
+      data: excelData,
+    };
+    // Make a POST request to the Excel API route
+    const response = await fetch("/api/excel/download", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Specify the content type if sending JSON data
+      },
+      body: JSON.stringify(postData),
+    });
+
+    if (response.ok) {
+      // Convert the response to a Blob and create a URL for downloading
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      // Create a download link and trigger the download
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "Excel.xlsx";
+      a.click();
+
+      // Clean up by revoking the URL
+      window.URL.revokeObjectURL(url);
+    } else {
+      console.error("Failed to generate Excel file.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
 export default function Page() {
-    const [file, setFile] = useState<any>(null);
-    const [excelData, setExcelData] = useState<any>([]);
+  const [file, setFile] = useState<any>(null);
+  const [excelData, setExcelData] = useState<any>([]);
+  const [basePath, setBasePath] = useState<String>();
+  const submitForm = (e: any) => {
+    e.preventDefault();
+    console.log(file);
 
-    const submitForm = (e: any) => {
-        e.preventDefault();
-        console.log(file);
+    const formData = new FormData();
+    formData.append("file", file);
+    axios
+      .post(basePath + "/api/excel/read", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res: any) => {
+        setExcelData(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    setBasePath(window.location.origin);
+  }, []);
 
-        const formData = new FormData();
-        formData.append("file", file);
-        axios.post("http://localhost:3000/api/excel/read", formData, {
-            headers: { "Content-Type": "multipart/form-data" }
-        }).then((res) => {
-            setExcelData(res.data.data);
-            console.log(res.data.data);
-        }
-        ).catch((err) => {
-            console.log(err);
-        })
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <input
+          type="file"
+          onChange={(e: any) => {
+            setFile(e.target.files[0]);
+          }}
+          required
+        />
+        <input type="submit" value={`Submit`} />
+      </form>
+      <button
+        className="bg-green-400"
+        onClick={() => downloadExcelFile(excelData)}
+      >
+        Download
+      </button>
+      <div>
+        {excelData.map((sheet: any, sheetIndex: any) => (
+          <div key={sheetIndex}>
+            <h2>{sheet.sheetName}</h2>
+            <table>
+              <thead>
+                <tr className="flex">
+                  {sheet.rows[0].cells.map((cell: any, cellIndex: any) => (
+                    <th key={cellIndex}>{cell.address}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {sheet.rows.map((row: any, rowIndex: any) => (
+                  <tr key={rowIndex}>
+                    {row.cells.map((cell: any, cellIndex: any) => (
+                      <td key={cellIndex}>
+                        {cell.formula ? cell.value.result : cell.value}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
+      <div>
+        {excelData.map((sheet: any, sheetIndex: any) => (
+          <div key={sheetIndex}>
+            <h2>{sheet.sheetName}</h2>
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  {sheet.headers.map((header: any, rowIndex: any) => (
+                    <th scope="col" className="px-6 py-3">
+                      {header.value}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
 
-    }
-
-    return <div>
-        <form onSubmit={submitForm}>
-            <input type="file" onChange={(e: any) => { setFile(e.target.files[0]) }} required />
-            <input type="submit" value={`Submit`} />
-        </form>
-        <div>
-            {excelData.map((sheet: any, sheetIndex: any) => (
-                <div key={sheetIndex}>
-                    <h2>{sheet.sheetName}</h2>
-                    <table >
-                        <thead>
-                            <tr className="flex">
-                                {sheet.rows[0].cells.map((cell: any, cellIndex: any) => (
-                                    <th key={cellIndex}>{cell.address}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {sheet.rows.map((row: any, rowIndex: any) => (
-                                <tr key={rowIndex}>
-                                    {row.cells.map((cell: any, cellIndex: any) => (
-                                        <td key={cellIndex}>{cell.formula ? cell.value.result : cell.value}</td>
-                                    ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            ))}
-        </div>
-        <div>
-            {excelData.map((sheet: any, sheetIndex: any) => (
-                <div key={sheetIndex}>
-                    <h2>{sheet.sheetName}</h2>
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                {sheet.headers.map((header: any, rowIndex: any) => (
-                                    <th scope="col" className="px-6 py-3">
-                                        {header}
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <tbody>
+                {/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 {sheet.rows.map((row: any, rowIndex: any) => (
                                     <td scope="col" className="px-6 py-3">
                                         {row.cells[0].value}
                                     </td>
                                 ))}
                             </tr> */}
-                            {sheet.rows.map((row: any, rowIndex: any) => (
-                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    {row.cells.map((cell: any, cellIndex: any) => (
-                                        <td key={cellIndex} scope="col" className="px-6 py-3">
-                                            {cell.formula ? cell.value.result : cell.value}
-                                        </td>
-                                    ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-
-
-            )
-
-            )
-            }
-        </div>
-
+                {sheet.rows.map((row: any, rowIndex: any) => (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    {row.cells.map((cell: any, cellIndex: any) => (
+                      <td key={cellIndex} scope="col" className="px-6 py-3">
+                        {cell.formula ? cell.value.result : cell.value}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
     </div>
-
+  );
 }
-
--- src/pages/api/excel/download.ts
+++ src/pages/api/excel/download.ts
@@ -1,13 +1,85 @@
-// var value:String = value.format?value.result :value
-const aff = [
-  {
-    value: "",
-    cellAddress:"",
-    format: {
-      color: "#0000",
-      bold: true,
-      italic: true,
-    },
-    formula:""
-  },
-];
+// pages/api/excel/download.ts
+// url: http://localhost:3000/api/excel/read
+
+import { NextApiRequest, NextApiResponse } from "next";
+import ExcelJS from "exceljs";
+import { Readable } from "stream";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.body.data);
+  const excelData: any = req.body.data;
+
+  var downloadableData: any = [];
+  excelData.map((sheet: any, index: number) => {
+    console.log(index);
+    downloadableData.push({
+      sheetName: "",
+      rows: [],
+    });
+
+    downloadableData[index].sheetName = sheet.sheetName;
+    sheet.rows.map((row: any) => {
+      row.cells.map((cell: any) => {
+        const format = cell.format;
+        var convertedData = {
+          value: cell.value,
+          address: cell.address,
+          format: {
+            font: format.font,
+            fill: format.fill,
+          },
+          formula: cell.formula,
+        };
+        downloadableData[index].rows.push(convertedData);
+      });
+    });
+    sheet.headers.map((header: any, i: number) => {
+      const format = header.format;
+      var convertedData = {
+        value: header.value,
+        address: header.address,
+        format: {
+          font: format.font,
+          fill: format.fill,
+        },
+        formula: header.formula,
+      };
+      downloadableData[index].rows.push(convertedData);
+    });
+  });
+
+  try {
+    // Create a new workbook
+    const workbook = new ExcelJS.Workbook();
+    downloadableData.map(async (data: any, index: number) => {
+      const worksheet = workbook.addWorksheet(data.sheetName);
+      data.rows.map((cell: any, index: number) => {
+        var address = worksheet.getCell(cell.address);
+        var format = cell.format;
+        address.value = cell.value;
+        address.style = { font: format.font, fill: format.fill };
+      });
+    });
+
+    // Generate the Excel file
+    const buffer = await workbook.xlsx.writeBuffer();
+
+    // Create a stream to generate the Excel file
+    const stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+
+    // Set the response headers to indicate an Excel file download
+    res.setHeader("Content-Disposition", 'attachment; filename="data.xlsx"');
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    // Pipe the stream to the response
+    stream.pipe(res);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ error: "Failed to generate Excel file." });
+  }
+};
