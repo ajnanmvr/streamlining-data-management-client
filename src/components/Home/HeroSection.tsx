39d1d7c9cecae6532f045fb0bd9d32616922d98d
Muhammed Ajnan P <94433947+ajnanmvr@users.noreply.github.com>
2023-11-14 20:55:51 +0000

    some chage

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -7,7 +7,7 @@ function HeroSection() {
                 <div>
                     <h1 className='text-center lg:text-left font-extrabold text-[39px] leading-[45px] lg:text-[70px] lg:w-5/6 lg:leading-[80px]'>Unlock the Power of <span className='text-primary'>Excelens</span>, Anywhere, Anytime!</h1>
                     <div className='flex gap-2 font-semibold justify-center lg:justify-start mt-8'>
-                        <Link href={"/project"}><button className='hover:bg-light lg:text-lg rounded-lg text-white px-3 py-2 bg-primary border-primary'>Get Started</button></Link>
+                        <Link href={"/sandbox"}><button className='hover:bg-light lg:text-lg rounded-lg text-white px-3 py-2 bg-primary border-primary'>Get Started</button></Link>
                         <Link href={"/docs"}><button className='hover:text-light border border-primary hover:border-light lg:text-lg rounded-lg text-primary px-3 py-2'>Read Docs</button></Link>
                     </div>
                 </div>
--- src/components/project/View.tsx
+++ src/components/project/View.tsx
@@ -4,50 +4,49 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React, { ReactNode, useEffect, useState } from 'react'
 
-interface Props{
-    id : string
+interface Props {
+  id: string;
 }
 
-const View = (props:Props) => {
+const View = (props: Props) => {
+  const [project, setProject] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(true); // New state for loading
+  const router = useRouter();
 
+  useEffect(() => {
+    const userId = props.id;
+    setLoading(true); // Set loading to true when starting the request
+    Axios.get(`/projects/${userId}`)
+      .then((res) => {
+        setProject(res.data);
+        setLoading(false); // Set loading to false when data is received
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false); // Set loading to false on error as well
+      });
+  }, [props.id]);
 
-    const [project , setProject ] = useState<any>()
-    const router = useRouter()
-
-    useEffect(() => {
-        const userId = props.id;
-        Axios.get(`/projects/${userId}`)
-          .then((res) => {
-            setProject(res.data);
-            console.log(res.data);
-            
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-    
-      }, []);
-
-      
-const DeleteProject = () => {
-  Axios.delete(`projects/${props.id}`)
-  .then((res) => {
-    console.log(res.data);
-    router.push('/projects')
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-}
-
-    
+  const DeleteProject = () => {
+    Axios.delete(`projects/${props.id}`)
+      .then((res) => {
+        console.log(res.data);
+        router.push('/projects');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <main className="font-sans h-screen overflow-hidden flex bg-accent">
-       {
-         project?.data?.map((sheet: any, sheetIndex: any) => (
-          <div
-            className=" flex flex-col items-center my-10 h-[400px]"
+    <main className="font-sans h-screen overflow-hidden flex flex-col bg-accent">
+      {loading ? (
+        <div className='w-screen flex justify-center'>     <img src="/loading.gif" alt="" className="mx-auto p-20 h-52" /></div>
+      ) : (
+        project?.data?.map((sheet: any, sheetIndex: any) => (
+          <><div
+            className="flex flex-col items-center my-10  w-screen"
             key={sheetIndex}
           >
             <hr className="border-2 border-dashed w-full border-primary mt-10 -mb-7" />
@@ -82,7 +81,7 @@ const DeleteProject = () => {
                   <td colSpan={5}>
                     <Link
                       href={`/project/${props.id}/edit`}
-                      // onClick={() => setToLocalStorage(excelData)}
+                    // onClick={() => setToLocalStorage(excelData)}
                     >
                       <h2 className="p-2 w-full text-primary font-semibold text-center">
                         Go to Editor
@@ -93,15 +92,15 @@ const DeleteProject = () => {
               </tbody>
             </table>
           </div>
-        ))}
-
-        <button onClick={()=>{
-          DeleteProject()
-        }}>
-          Delete Project
-        </button>
+            <button className='border-red-700 border rounded-lg text-red-700 font-semibold hover:text-white px-3 py-1 hover:bg-red-700 flex items-center gap-1 w-fit mx-auto fill-red-700 hover:fill-white' onClick={() => {
+              DeleteProject()
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg>Delete Project
+            </button></>
+        ))
+      )}
     </main>
   );
-}
+};
 
-export default View
+export default View;
