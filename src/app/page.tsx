fbe16ecde86153f77755f8fec48ba2499472dca7
Muhammed Zulfikar <99268671+zzulfi@users.noreply.github.com>
2023-11-14 11:34:37 +0000

    Remove duplicate DemoSection and update descriptions and features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,6 @@ export default function Home() {
         <HeroSection />
       </div>
       <DemoSection />
-      <DemoSection />
-      <DemoSection />
-      <DemoSection />
       <Footer />
     </main>
   )
